b4bd7114baaa04621aa27da1bd2e24f35dcfcb56
AnnaShaleva <anna.shaleva98@mail.ru>
2019-03-07 16:03:31 +0300

    Task 1: fixes

--- task1/integral.hh
+++ task1/integral.hh
@@ -3,24 +3,28 @@ double function(double x)
 	return 4.0/(1 + x * x);
 }
 
-double integral(int thread_num, bool reduct)
+double integral(bool is_parallel)
 {
 	double a = .0;
 	double b = 1.0;
-	int n = 100000;
+	//int n = 100000;
+	int n = 10;
 	double h = (b - a)/n;
 	double x[n + 1];
 	double f[n + 1];
 	double sum = 0.0;
-#pragma omp parallel num_threads(thread_num) if (reduct) reduction(+: sum)
-#pragma omp parallel for
-	for (int i = 0; i < n + 1; i++)
-	{
-		x[i] = a + (b - a) * i / n;
-		f[i] = function(x[i]);
-		sum = sum + f[i];
-	}
 
+	#pragma omp parallel 
+	{
+		std::cout << "Thread number: " << omp_get_thread_num() << std::endl;
+		#pragma omp for
+		for (int i = 0; i < n + 1; i++)
+		{
+			x[i] = a + (b - a) * i / n;
+			f[i] = function(x[i]);
+			sum = sum + f[i];
+		}
+	}
 	sum = h*sum;
 
 	return sum;
--- task1/main.cpp
+++ task1/main.cpp
@@ -1,32 +1,24 @@
+#include "omp.h"
 #include <iostream>
 #include <chrono>
 #include "integral.hh"
-#include "omp.h" 
+ 
 
 int main()
 {
 	auto start1 = std::chrono::system_clock::now();
-	double result1 = integral(1, false);
+	double result1 = integral(true);
 	auto end1 = std::chrono::system_clock::now();
 
 	auto start2 = std::chrono::system_clock::now();
-	double result2 = integral(10, false);
+	double result2 = integral(false);
 	auto end2 = std::chrono::system_clock::now();
 
-	auto start3 = std::chrono::system_clock::now();
-	double result3 = integral(10, true);
-	auto end3 = std::chrono::system_clock::now();
-
-
-
 	std::cout << "result1: " << result1 << std::endl;
-	std::cout << "Single thread integral: " << std::chrono::duration_cast<std::chrono::microseconds>(end1 - start1).count() << "microseconds" << std::endl;
+	std::cout << "Single thread integral: " << std::chrono::duration_cast<std::chrono::microseconds>(end1 - start1).count() << " microseconds" << std::endl;
 
 	std::cout << "result2: " << result2 << std::endl;
-	std::cout << "Multy thread integral: " << std::chrono::duration_cast<std::chrono::microseconds>(end2 - start2).count() << "microseconds" << std::endl;
+	std::cout << "Multy thread integral: " << std::chrono::duration_cast<std::chrono::microseconds>(end2 - start2).count() << " microseconds" << std::endl;
 
-	std::cout << "result3: " << result3 << std::endl;
-	std::cout << "Multy thread integral with reduction: " << std::chrono::duration_cast<std::chrono::microseconds>(end3 - start3).count() << "microseconds" << std::endl;
-	
 	return 0;
 }
