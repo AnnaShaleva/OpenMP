3f658b24ca642a5bda128c20f3631be4c08feb39
Anna <shaleva.ann@gmail.com>
2019-03-10 16:32:20 +0300

    Task 5: Added comparative table

--- task5/main.cpp
+++ task5/main.cpp
@@ -3,62 +3,66 @@
 #include <chrono>
 #include "product.hh"
 #include <ctime>
+#include <fstream>
 
 int main()
 {
+	std::ofstream outfile;
+	outfile.open("out.txt");
+	 
 	std::srand(unsigned(std::time(0)));
-	int m = 10000;
-	int n = 10000;
-
-	int** a = new int*[m];
-	for(int i = 0; i < m; i++)
-	{
-		a[i] = new int[n];
-		for(int j = 0; j < n; j++)
-		{
-			a[i][j] = std::rand() % 10;
-			//std::cout << a[i][j] << " ";
-		}
-		//std::cout << std::endl;
-	}
-	//std::cout << std::endl;
-	int* b = new int[n];
-	for(int i = 0; i < n; i++)
-	{
-		b[i] = std::rand() % 10;
-		//std::cout << b[i] << " ";
-	}
-	//std::cout << std::endl;
 	
-	int thread_num1 = 1;		
-	int thread_num2 = 5;
-	auto start1 = std::chrono::system_clock::now();
-	int* result1 = row_product(a, b, m, n, thread_num1);
-	auto end1 = std::chrono::system_clock::now();
+	int max_threads_number = 15;
+	int max_dimention = 100000;
 
-	auto start2 = std::chrono::system_clock::now();
-	int* result2 = row_product(a, b, m, n, thread_num2);
-	auto end2 = std::chrono::system_clock::now();
+	for(int dim = 10; dim < max_dimention; dim*=10)
+		outfile << dim << " ";
+	outfile << std::endl;
+	for(int threads_number = 1; threads_number < max_threads_number+1; threads_number++)
+	{
+		std::cout << "Threads number: " << threads_number << std::endl;
+		for(int dimention = 10; dimention < max_dimention; dimention *= 10)
+		{
+			std::cout << "Dimention: " << dimention << std::endl;
+			int m = dimention;
+			int n = dimention;
+			int** a = new int*[m];
+			for(int i = 0; i < m; i++)
+			{
+				a[i] = new int[n];
+				for(int j = 0; j < n; j++)
+				{
+					a[i][j] = std::rand() % 10;
+					//std::cout << a[i][j] << " ";
+				}
+				//std::cout << std::endl;
+			}
+			//std::cout << std::endl;
+			int* b = new int[n];
+			for(int i = 0; i < n; i++)
+			{
+				b[i] = std::rand() % 10;
+				//std::cout << b[i] << " ";
+			}
+			//std::cout << std::endl;
+			
+			auto start = std::chrono::system_clock::now();
+			int* result = row_product(a, b, m, n, threads_number);
+			auto end = std::chrono::system_clock::now();
 
-	//std::cout << "result1: " << std::endl;
-	//for(int i = 0; i < m; i++)
-	//{
-		//std::cout << result1[i] << " ";
-	//}
-	//std::cout << std::endl;
+			outfile << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " ";
 
-	std::cout << "Single thread algorithm: " << std::chrono::duration_cast<std::chrono::microseconds>(end1 - start1).count() << " microseconds" << std::endl;
-
-	//std::cout << "result2: " << std::endl;
-	//for(int i = 0; i < m; i++)
-	//{
-	//	std::cout << result2[i] << " ";
-	//}
-	//std::cout << std::endl;
-
-	std::cout << "Multy thread algorithm: " << std::chrono::duration_cast<std::chrono::microseconds>(end2 - start2).count() << " microseconds" << std::endl;
-
-
+			for(int i =0; i < m; i++)
+			{
+				delete[] a[i];
+			}
+			delete[] a;
+			delete[] b;
+			delete[] result;
+		}
+		outfile << std::endl;
+	}
+	outfile.close();
 	return 0;
 }
 
